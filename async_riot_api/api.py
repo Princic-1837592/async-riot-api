e5b53a5a0b5e126434b9752cb5dfe49bebc2e140
Princic-1837592 <andreapri.99@gmail.com>
2021-12-28 00:29:05 +0100

    more tests about docs

--- async_riot_api/api.py
+++ async_riot_api/api.py
@@ -10,18 +10,6 @@ from . import types
 class LoLAPI:
     """Main class to interact with the API. Offers async methods corresponding to API methods and more.
     
-    It is important to notice that this implementation is exception-free, not meaning that it's impossible to make mistake,
-    but meaning that errors returned by the API are not raised as exceptions. Instead, they are returned as `types.RiotApiError`,
-    containing information about the error.
-    To distinguish between a successful response and an error, you can easily use the object as a boolean expression:
-    .. highlight:: python
-    .. code-block:: python
-        summoner = await api.get_account_by_puuid(puuid)
-        if not summoner:
-            print(f'The request raised an error with status code {summoner.status_code}: {summoner.message}')
-        else:
-            print(summoner.to_string(sep = '|   '))
-
     Parameters:
         api_key (``str``): your API token
         
