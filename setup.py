274d3a3fa113f3641f196cab30c1d472b4e33152
Princic-1837592 <andreapri.99@gmail.com>
2021-12-27 20:51:14 +0100

    more tests about docs

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = 'async-riot-api',
+    name = 'async_riot_api',
     version = '0.0.26',
     author = 'Andrea Princic',
     author_email = 'princic.1837592@studenti.uniroma1.it',
